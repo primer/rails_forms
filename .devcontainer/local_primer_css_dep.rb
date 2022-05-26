813e27d1e3512b039e0c7e1cc570c9b48dd08c03
Cameron Dutro <camertron@gmail.com>
2022-05-26 20:40:44 +0000

    Get monospace working

--- .devcontainer/local_primer_css_dep.rb
+++ .devcontainer/local_primer_css_dep.rb
@@ -2,7 +2,7 @@
 
 require "json"
 
-package_json_path = "lookbook/package.json"
+package_json_path = File.expand_path("../lookbook/package.json", __dir__)
 package_json = JSON.parse(File.read(package_json_path))
 package_json["dependencies"]["@primer/css"] = "file:/workspaces/@primer/css"
-File.write(package_json_path, package_json.to_json)
+File.write(package_json_path, JSON.pretty_generate(package_json))
--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -3,7 +3,32 @@
 script_folder="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 workspaces_folder="$(cd "${script_folder}/../.." && pwd)"
 
-cd "${workspaces_folder}"
-git clone "https://github.com/primer/css" @primer/css
+echo "##### Pointing Lookbook to local copy of @primer/css"
+/usr/bin/env ruby .devcontainer/local_primer_css_dep.rb
 
-/usr/bin/env ruby .devcontainer/post-create-command.rb
+cd "${workspaces_folder}"
+
+echo "##### Cloning @primer/css"
+if [[ ! -d "@primer/css" ]]; then
+  git clone "https://github.com/primer/css" @primer/css
+fi
+
+echo "##### Adding index.scss stub"
+echo '@import "./src";' > @primer/css/index.scss
+echo 'index.scss' >> @primer/css/.git/info/exclude
+
+cd "${workspaces_folder}/rails_forms"
+
+echo "##### Installing ruby dependencies in rails_forms"
+bundle install --jobs 3 --retry 4
+
+echo "##### Installing JavaScript dependencies in rails_forms"
+yarn install
+
+cd lookbook
+
+echo "##### Installing ruby dependencies in rails_forms/lookbook"
+bundle install --jobs 3 --retry 4
+
+echo "##### Installing JavaScript dependencies in rails_forms/lookbook"
+yarn install
